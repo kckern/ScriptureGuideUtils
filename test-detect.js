3d27a4bf81d7a82a5dcff762dde831762c8fd15a
KC Kern <kc@kckern.com>
2024-01-26 09:34:10 -0500

    Test improvements

--- test-detect.js
+++ test-detect.js
@@ -1,38 +1,34 @@
-const { ko } = require("./data/scriptlang.js");
-const {lookup, generateReference, setLanguage, detectReferences}= require("./scriptures.js");
+
+const { setLanguage, detectReferences}= require("./scriptures.js");
+const fs = require("fs");
+const paramLang = process.argv[2];
+const langs = fs.readdirSync(`${__dirname}/test/data`)
+                .filter(i=>/txt$/.test(i))
+                .map(i=>i.replace(".txt",""))
+                .filter(i=>{
+                    if(!paramLang) return true;
+                    if(paramLang && i===paramLang) return true
+                    return false;
+                });
+
+let callback = (string)=>{
+    return `[${string}]`
+}
 
 
-const lines = [
-    "Please refer to Genesis 20 in the bible",
-    "Please refer to 3 Nephi 5:1-3 in the Book of Mormon",
-    "For example, in Matthew 27:51, the earth quakes and ro",
-    "It aligns with other scriptures (3 Nephi 12:2, Mosiah 18:13, Moses 6:52).",
-]
-
-lines.forEach(l=>{
-    const result = detectReferences(l);
-    console.log({result});
-});
-
-process.exit(0);
-
-setLanguage("ko");
-
-const ko_lines = [
-    "성경의 창세기 20장을 참조하십시오.",
-    "이제 제3니파이서 5:1-3을 참조하십시오.",
-    "이제 제4니파이서 1:4~5절 참조하십시오.",
-    "이제 제4니파이서 1장4~5절 참조하십시오.",
-    "이제 제4니파이서 1장 4~5절 참조하십시오.",
-    "이제 제4니파이서 1.4~5절 참조하십시오.",
-    " 관련하여 힐라맨서 5장 24절에서는 불기둥이 불을 일으키지 않고",
-    "이제 모세서 3장 참조하십시오.",
-    "이제 니파이후서 12:5; 예레미야서 2:5; 앨마서 5:37 참조하십시오.",
-]
-ko_lines.forEach(l=>{
-    let callback = (string)=>{
-        return `[${string}]`
+for(const lang of langs){
+    if(lang!=="en") setLanguage(lang);
+    let lines = fs.readFileSync(`${__dirname}/test/data/${lang}.txt`,"utf-8").split("\n").map(i=>i.trim()).filter(x=>!!x);
+    const hasAsterix = lines.some(i=>/^[*]/.test(i));
+    if(hasAsterix) lines = lines.filter(i=>/^[*]/.test(i)).map(i=>i.replace(/^[*]/,""))
+    for(const line of lines){
+        const i = detectReferences(line,callback);
+        console.log([i]);
     }
-    const result = detectReferences(l,callback);
-    console.log({result});
-});
+}
+
+
+
+
+
+
